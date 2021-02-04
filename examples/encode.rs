e21b3c92f178dcfd064ae46dd79fbf978731f5fe
Tiago Carvalho <sugoiuguu@tfwno.gf>
2021-02-04 21:14:46 +0000

    draw circle

--- examples/encode.rs
+++ examples/encode.rs
@@ -15,15 +15,25 @@ fn main() -> io::Result<()> {
     let mut stdout_writer = BufWriter::new(stdout_lock);
 
     let mut image = RawPixBuf::new(DIM, DIM);
-    colorize(image.as_typed_mut());
+    draw_image(image.as_typed_mut());
 
     Farbfeld.encode(&mut stdout_writer, &&mut image)
 }
 
-fn colorize(buf: &mut [Nrgba]) {
+fn draw_image(buf: &mut [Nrgba]) {
+    const HALF: usize = DIM / 2;
+    const RED: Nrgba = Nrgba { r: 255, g: 0, b: 0, a: 255 };
+    const WHITE: Nrgba = Nrgba { r: 255, g: 255, b: 255, a: 255 };
+
     for y in 0..DIM {
+        let yh = y - HALF;
         for x in 0..DIM {
-            buf[y*DIM + x] = Nrgba { r: 255, g: 0, b: 0, a: 255 };
+            let xh = x - HALF;
+            if xh*xh + yh*yh < HALF*HALF {
+                buf[y*DIM + x] = RED;
+            } else {
+                buf[y*DIM + x] = WHITE;
+            }
         }
     }
 }
