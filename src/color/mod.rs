e426a3cedd9924e3baec67656dd6f994b36aace2
Tiago Carvalho <sugoiuguu@tfwno.gf>
2021-02-04 19:59:20 +0000

    color stuff

--- src/color/mod.rs
+++ src/color/mod.rs
@@ -1,3 +1,17 @@
+mod rgb;
+mod cmyk;
+mod nrgba;
+mod nrgba64;
+mod gray;
+mod gray16;
+
+pub use rgb::*;
+pub use cmyk::*;
+pub use nrgba::*;
+pub use nrgba64::*;
+pub use gray::*;
+pub use gray16::*;
+
 pub trait Color {
     /// Return an alpha premultiplied color in RGBA 16 bits.
     fn as_rgba(&self) -> (u32, u32, u32, u32);
@@ -9,14 +23,6 @@ pub struct Nrgba64 {
     pub g: u16,
     pub b: u16,
     pub a: u16,
-}
-
-#[derive(Copy, Clone, Eq, PartialEq, Debug)]
-pub struct Nrgba {
-    pub r: u8,
-    pub g: u8,
-    pub b: u8,
-    pub a: u8,
 }
 
 #[derive(Copy, Clone, Eq, PartialEq, Debug)]
@@ -42,4 +48,19 @@ pub struct Gray {
 #[derive(Copy, Clone, Eq, PartialEq, Debug)]
 pub struct Gray16 {
     pub y: u16,
+}
+
+impl Color for Nrgba64 {
+    fn as_rgba(&self) -> (u32, u32, u32, u32) {
+        let r = self.r as u32;
+        let g = self.g as u32;
+        let b = self.b as u32;
+        let a = self.a as u32;
+        
+        let r = (r * a) / 0xffff;
+        let g = (g * a) / 0xffff;
+        let b = (b * a) / 0xffff;
+        
+        (r, g, b, a)
+    }
 }
---
+++ src/color/nrgba/mod.rs
@@ -0,0 +1,37 @@
+use super::Color;
+
+#[derive(Copy, Clone, Eq, PartialEq, Debug)]
+pub struct Nrgba {
+    pub r: u8,
+    pub g: u8,
+    pub b: u8,
+    pub a: u8,
+}
+
+impl Color for Nrgba {
+    fn as_rgba(&self) -> (u32, u32, u32, u32) {
+        let r = self.r as u32;
+        let g = self.g as u32;
+        let b = self.b as u32;
+        let a = self.a as u32;
+        
+        let r = r | (r << 8);
+        let g = g | (g << 8);
+        let b = b | (b << 8);
+        let a = a | (a << 8);
+        
+        (r, g, b, a)
+    }
+}
+
+impl<C: Color> From<&C> for Nrgba {
+    fn from(c: &C) -> Nrgba {
+        let (r, g, b, a) = c.as_rgba();
+        Nrgba {
+            r: ((r >> 8) & 0xff) as u8,
+            g: ((g >> 8) & 0xff) as u8,
+            b: ((b >> 8) & 0xff) as u8,
+            a: ((a >> 8) & 0xff) as u8,
+        }
+    }
+}
