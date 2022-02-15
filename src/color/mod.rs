9bab54fa15ba5d620613a92c396bbd676ac3934c
Tiago Carvalho <sugoiuguu@tfwno.gf>
2022-02-15 00:56:52 +0000

    endianness comments and derives

--- src/color/mod.rs
+++ src/color/mod.rs
@@ -16,14 +16,19 @@ pub use gray16::*;
 
 pub trait Color {
     /// Return an alpha premultiplied color in RGBA 16 bits.
+    ///
+    /// Components are returned as native-endian values.
     fn as_rgba(&self) -> (u32, u32, u32, u32);
 }
 
 /// Tag a pixel's channel value as big-endian.
+#[derive(Copy, Clone, Eq, PartialEq, Debug)]
 pub enum BigEndian {}
 
 /// Tag a pixel's channel value as little-endian.
+#[derive(Copy, Clone, Eq, PartialEq, Debug)]
 pub enum LittleEndian {}
 
 /// Tag a pixel's channel value as native-endian.
+#[derive(Copy, Clone, Eq, PartialEq, Debug)]
 pub enum NativeEndian {}
