66698a88c6f2947de605c64325bcbe89c2d14560
Tiago Carvalho <sugoiuguu@tfwno.gf>
2022-10-29 04:20:01 +0100

    improvements

--- scripts/blur_many.sh
+++ scripts/blur_many.sh
@@ -21,8 +21,7 @@ img=${1:-'-'}
 cat $img | ./target/release/toff > $final
 
 for i in $(seq 1 $n); do
-    cat $final | ./target/release/blur > $intermediate
-    rm $final
+    $final < ./target/release/blur > $intermediate
     mv $intermediate $final
 done
 
