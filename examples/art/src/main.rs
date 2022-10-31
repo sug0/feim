0df8ddfb8ce5c20ea104d0c6fa526fe7c5a477e1
Tiago Carvalho <sugoiuguu@tfwno.gf>
2022-10-31 21:53:05 +0000

    change art example to use new image color set

--- examples/art/src/main.rs
+++ examples/art/src/main.rs
@@ -1,8 +1,9 @@
 use std::io::{self, BufWriter};
 
-use feim::buffer::{AsTypedMut, RawPixBuf};
+use feim::buffer::RawPixBuf;
 use feim::color::Gray;
 use feim::image::jpeg::{Jpeg, JpegEncodeOptions};
+use feim::image::ImageMut;
 use feim::serialize::Encode;
 
 const DIM: usize = 2000;
@@ -14,7 +15,7 @@ fn main() -> io::Result<()> {
 
     let image = {
         let mut image = RawPixBuf::new(DIM, DIM);
-        draw_image(image.as_typed_mut());
+        draw_image(&mut image);
         image
     };
 
@@ -26,7 +27,7 @@ const fn shade(y: u8) -> Gray {
     Gray { y }
 }
 
-fn draw_image(buf: &mut [Gray]) {
+fn draw_image(buf: &mut RawPixBuf<Gray>) {
     for y in 0..DIM {
         let yf = y as f32;
         for x in 0..DIM {
@@ -34,7 +35,7 @@ fn draw_image(buf: &mut [Gray]) {
             let value = 255.0 * (xf - std::f32::consts::PI * xf).sin();
             let v = ((value as usize) << (x ^ !y))
                 ^ (value.cos().mul_add(yf, std::f32::consts::PI * xf.cos()) as usize);
-            buf[y * DIM + x] = shade((v & 0xff) as u8);
+            buf.pixel_set(x, y, shade((v & 0xff) as u8));
         }
     }
 }
