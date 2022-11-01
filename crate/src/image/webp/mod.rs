88bf85c549f868b67777c49b0457b93fd506c5e4
Tiago Carvalho <sugoiuguu@tfwno.gf>
2022-11-01 21:15:42 +0000

    webp buf

--- crate/src/image/webp/mod.rs
+++ crate/src/image/webp/mod.rs
@@ -1,8 +1,7 @@
-pub use webp::WebPImage as WebpBuf;
+mod webp_buf;
 
+pub use self::webp_buf::*;
 use crate::impl_format;
-//use crate::image::{Dimensions, Image, ImageMut};
-use crate::image::Dimensions;
 
 pub struct Webp;
 
@@ -10,14 +9,4 @@ impl_format! {
     name: Webp,
     id: "feim:webp",
     magic: b"RIFF????WEBPVP8",
-}
-
-impl Dimensions for WebpBuf {
-    fn width(&self) -> usize {
-        self.width() as usize
-    }
-
-    fn height(&self) -> usize {
-        self.height() as usize
-    }
 }
---
+++ crate/src/image/webp/webp_buf/mod.rs
@@ -0,0 +1,52 @@
+pub use webp::WebPImage as WebpBuf;
+
+//use crate::image::{Dimensions, Image, ImageMut};
+use crate::color::{Color, Nrgba, Rgb};
+use crate::image::{Dimensions, Image};
+
+#[derive(Copy, Clone, Debug)]
+pub enum WebpPix {
+    Nrgba(Nrgba),
+    Rgb(Rgb),
+}
+
+impl Color for WebpPix {
+    fn as_rgba(&self) -> (u32, u32, u32, u32) {
+        match self {
+            WebpPix::Nrgba(c) => c.as_rgba(),
+            WebpPix::Rgb(c) => c.as_rgba(),
+        }
+    }
+}
+
+impl Dimensions for WebpBuf {
+    #[inline]
+    fn width(&self) -> usize {
+        self.width() as usize
+    }
+
+    #[inline]
+    fn height(&self) -> usize {
+        self.height() as usize
+    }
+}
+
+impl Image for WebpBuf {
+    type Pixel = WebpPix;
+
+    fn color_get(&self, x: usize, y: usize) -> Self::Pixel {
+        let stride = if self.is_alpha() { 4 } else { 3 };
+        let index = (y * Dimensions::width(self) + x) * stride;
+
+        let r = self[index];
+        let g = self[index + 1];
+        let b = self[index + 2];
+
+        if stride == 4 {
+            let a = self[index + 3];
+            WebpPix::Nrgba(Nrgba { r, g, b, a })
+        } else {
+            WebpPix::Rgb(Rgb { r, g, b })
+        }
+    }
+}
