ffec95cf963a576ef94bcecdcc432e444790c14b
Tiago Carvalho <sugoiuguu@tfwno.gf>
2022-11-01 21:03:35 +0000

    webp dims

--- crate/src/image/webp/mod.rs
+++ crate/src/image/webp/mod.rs
@@ -1,4 +1,8 @@
+pub use webp::WebPImage as WebpBuf;
+
 use crate::impl_format;
+//use crate::image::{Dimensions, Image, ImageMut};
+use crate::image::Dimensions;
 
 pub struct Webp;
 
@@ -6,4 +10,14 @@ impl_format! {
     name: Webp,
     id: "feim:webp",
     magic: b"RIFF????WEBPVP8",
+}
+
+impl Dimensions for WebpBuf {
+    fn width(&self) -> usize {
+        self.width() as usize
+    }
+
+    fn height(&self) -> usize {
+        self.height() as usize
+    }
 }
