4b6c73be2e5e805ada9e8a0126ab4df4ea01310c
Tiago Carvalho <sugoiuguu@tfwno.gf>
2022-11-01 16:08:34 +0000

    generic encode and decode

--- crate/src/serialize/mod.rs
+++ crate/src/serialize/mod.rs
@@ -1,7 +1,9 @@
 use std::io::{self, BufRead, Read, Write};
 
 use crate::image::Format;
-use crate::specialized::Aye;
+use crate::specialized::{Aye, No};
+
+// -------------------------------------------------------------------------- //
 
 pub trait EncodeOptions {
     type Options;
@@ -11,9 +13,13 @@ pub trait DecodeOptions {
     type Options;
 }
 
+// -------------------------------------------------------------------------- //
+
 pub trait Encode<B, Specialized = Aye>: EncodeOptions {
     fn encode<W: Write>(w: W, opts: Self::Options, buf: &B) -> io::Result<()>;
 }
+
+// -------------------------------------------------------------------------- //
 
 pub trait EncodeSpecialized<B>: EncodeOptions {
     fn encode_specialized<W: Write>(w: W, opts: Self::Options, buf: &B) -> io::Result<()>;
@@ -26,9 +32,26 @@ impl<B, F: Encode<B, Aye>> EncodeSpecialized<B> for F {
     }
 }
 
+// -------------------------------------------------------------------------- //
+
+pub trait EncodeGeneric<B>: EncodeOptions {
+    fn encode_generic<W: Write>(w: W, opts: Self::Options, buf: &B) -> io::Result<()>;
+}
+
+impl<B, F: Encode<B, No>> EncodeGeneric<B> for F {
+    #[inline]
+    fn encode_generic<W: Write>(w: W, opts: Self::Options, buf: &B) -> io::Result<()> {
+        Self::encode(w, opts, buf)
+    }
+}
+
+// -------------------------------------------------------------------------- //
+
 pub trait Decode<B, Specialized = Aye>: DecodeOptions {
     fn decode<R: Read>(r: R, opt: Self::Options) -> io::Result<B>;
 }
+
+// -------------------------------------------------------------------------- //
 
 pub trait DecodeSpecialized<B>: DecodeOptions {
     fn decode_specialized<R: Read>(r: R, opt: Self::Options) -> io::Result<B>;
@@ -40,6 +63,21 @@ impl<B, F: Decode<B, Aye>> DecodeSpecialized<B> for F {
         Self::decode(r, opt)
     }
 }
+
+// -------------------------------------------------------------------------- //
+
+pub trait DecodeGeneric<B>: DecodeOptions {
+    fn decode_generic<R: Read>(r: R, opt: Self::Options) -> io::Result<B>;
+}
+
+impl<B, F: Decode<B, No>> DecodeGeneric<B> for F {
+    #[inline]
+    fn decode_generic<R: Read>(r: R, opt: Self::Options) -> io::Result<B> {
+        Self::decode(r, opt)
+    }
+}
+
+// -------------------------------------------------------------------------- //
 
 pub fn try_format<R: BufRead>(mut r: R, formats: &[&dyn Format]) -> io::Result<usize> {
     let buf = r.fill_buf()?;
