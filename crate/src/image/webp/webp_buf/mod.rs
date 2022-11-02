36f35dd4d22635454aedc45bc1cd493d92c78610
Tiago Carvalho <sugoiuguu@tfwno.gf>
2022-11-02 23:21:50 +0000

    webp img and img mut

--- crate/src/image/webp/webp_buf/mod.rs
+++ crate/src/image/webp/webp_buf/mod.rs
@@ -1,63 +1,105 @@
-pub use webp::WebPImage as WebpBuf;
+use either::*;
+use webp::WebPImage;
 
 use crate::color::convert::ConvertInto;
 use crate::color::{Color, Nrgba, Rgb};
 use crate::image::{Dimensions, Image, ImageMut};
 
-#[derive(Copy, Clone, Debug)]
-pub enum WebpPix {
-    Nrgba(Nrgba),
-    Rgb(Rgb),
+pub trait WebpFeimExt {
+    fn into_feim(self) -> Either<RgbWebpBuf, NrgbaWebpBuf>;
 }
 
-impl Color for WebpPix {
-    fn as_rgba(&self) -> (u32, u32, u32, u32) {
-        match self {
-            WebpPix::Nrgba(c) => c.as_rgba(),
-            WebpPix::Rgb(c) => c.as_rgba(),
+impl WebpFeimExt for WebPImage {
+    fn into_feim(self) -> Either<RgbWebpBuf, NrgbaWebpBuf> {
+        if self.is_alpha() {
+            Right(NrgbaWebpBuf { inner: self })
+        } else {
+            Left(RgbWebpBuf { inner: self })
         }
     }
 }
 
-impl Dimensions for WebpBuf {
+pub type RgbWebpBuf = WebpBuf<false>;
+
+pub type NrgbaWebpBuf = WebpBuf<true>;
+
+pub struct WebpBuf<const HAS_ALPHA: bool> {
+    inner: WebPImage,
+}
+
+impl<const HAS_ALPHA: bool> Dimensions for WebpBuf<HAS_ALPHA> {
     #[inline]
     fn width(&self) -> usize {
-        self.width() as usize
+        self.inner.width() as usize
     }
 
     #[inline]
     fn height(&self) -> usize {
-        self.height() as usize
+        self.inner.height() as usize
     }
 }
 
-impl Image for WebpBuf {
-    type Pixel = WebpPix;
+impl Image for RgbWebpBuf {
+    type Pixel = Rgb;
 
     fn color_get(&self, x: usize, y: usize) -> Self::Pixel {
-        let stride = if self.is_alpha() { 4 } else { 3 };
-        let index = (y * Dimensions::width(self) + x) * stride;
+        let index = (y * Dimensions::width(self) + x) * 3;
 
-        let r = self[index];
-        let g = self[index + 1];
-        let b = self[index + 2];
+        let r = self.inner[index];
+        let g = self.inner[index + 1];
+        let b = self.inner[index + 2];
 
-        if stride == 4 {
-            let a = self[index + 3];
-            WebpPix::Nrgba(Nrgba { r, g, b, a })
-        } else {
-            WebpPix::Rgb(Rgb { r, g, b })
-        }
+        Rgb { r, g, b }
     }
 }
 
-impl ImageMut for WebpBuf {
-    type Pixel = WebpPix;
+impl Image for NrgbaWebpBuf {
+    type Pixel = Nrgba;
 
-    fn color_set<C, ColorSpecialized>(&mut self, _x: usize, _y: usize, _color: C)
+    fn color_get(&self, x: usize, y: usize) -> Self::Pixel {
+        let index = (y * self.width() + x) * 4;
+
+        let r = self.inner[index];
+        let g = self.inner[index + 1];
+        let b = self.inner[index + 2];
+        let a = self.inner[index + 3];
+
+        Nrgba { r, g, b, a }
+    }
+}
+
+impl ImageMut for RgbWebpBuf {
+    type Pixel = Rgb;
+
+    fn color_set<C, ColorSpecialized>(&mut self, x: usize, y: usize, color: C)
     where
-        C: ConvertInto<WebpPix, ColorSpecialized> + Color,
+        C: ConvertInto<Rgb, ColorSpecialized> + Color,
     {
-        todo!()
+        let index = (y * self.width() + x) * 3;
+        let color = <_ as ConvertInto<Rgb, ColorSpecialized>>::convert_into(color);
+
+        self.inner[index] = color.r;
+        self.inner[index + 1] = color.g;
+        self.inner[index + 2] = color.b;
     }
 }
+
+impl ImageMut for NrgbaWebpBuf {
+    type Pixel = Nrgba;
+
+    fn color_set<C, ColorSpecialized>(&mut self, x: usize, y: usize, color: C)
+    where
+        C: ConvertInto<Nrgba, ColorSpecialized> + Color,
+    {
+        let index = (y * Dimensions::width(self) + x) * 4;
+        let color = <_ as ConvertInto<Nrgba, ColorSpecialized>>::convert_into(color);
+
+        self.inner[index] = color.r;
+        self.inner[index + 1] = color.g;
+        self.inner[index + 2] = color.b;
+        self.inner[index + 3] = color.a;
+    }
+}
+
+// TODO: as ref / as ref mut for [u8]
+// TODO: as typed / as typed mut for rgb and nrgba
