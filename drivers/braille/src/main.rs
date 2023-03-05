85ae08dbac26c9108bcaa82caa46451b45f5551e
Tiago Carvalho <sugoiuguu@tfwno.gf>
2023-03-05 20:16:16 +0000

    fix braille

--- drivers/braille/src/main.rs
+++ drivers/braille/src/main.rs
@@ -1,5 +1,6 @@
 use std::io::{self, BufReader, BufWriter, Write};
 
+use clap::Parser;
 use feim::buffer::RawPixBuf;
 use feim::color::convert::ConvertInto;
 use feim::color::{Gray, Nrgba64Be};
@@ -8,7 +9,20 @@ use feim::image::{Dimensions, Image};
 use feim::serialize::Decode;
 use feim::specialized;
 
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct CmdLineArgs {
+    /// The luminance value
+    #[arg(short, long, default_value_t = 128)]
+    luminance: u8,
+    /// Whether the luminance value shall be inverted
+    #[arg(short, long, default_value_t = false)]
+    inverted: bool,
+}
+
 fn main() -> io::Result<()> {
+    let args = CmdLineArgs::parse();
+
     let stdin = io::stdin();
     let stdin_lock = stdin.lock();
     let stdin_reader = BufReader::new(stdin_lock);
@@ -21,43 +35,47 @@ fn main() -> io::Result<()> {
         check_header: false,
     };
     let image = Farbfeld::decode(stdin_reader, opts)?;
-    braille(image, stdout_writer)
+    args.braille(image, stdout_writer)
 }
 
-fn braille<W: Write>(im: RawPixBuf<Nrgba64Be>, mut w: W) -> io::Result<()> {
-    for y in (0..im.height()).step_by(4) {
-        for x in (0..im.width()).step_by(2) {
-            let mut bits = 0u32;
-            let mut output = [0u8; 4];
+impl CmdLineArgs {
+    fn braille<W: Write>(&self, im: RawPixBuf<Nrgba64Be>, mut w: W) -> io::Result<()> {
+        for y in (0..im.height()).step_by(4) {
+            for x in (0..im.width()).step_by(2) {
+                let mut bits = 0u32;
+                let mut output = [0u8; 4];
 
-            bits |= gray_at(&im, x, y);
-            bits |= gray_at(&im, x + 1, y) << 3;
-            bits |= gray_at(&im, x, y + 1) << 1;
-            bits |= gray_at(&im, x + 1, y + 1) << 4;
-            bits |= gray_at(&im, x, y + 2) << 2;
-            bits |= gray_at(&im, x + 1, y + 2) << 5;
-            bits |= gray_at(&im, x, y + 3) << 6;
-            bits |= gray_at(&im, x + 1, y + 3) << 7;
-            bits |= 0x2800;
+                bits |= self.lum_at(&im, x, y);
+                bits |= self.lum_at(&im, x + 1, y) << 3;
+                bits |= self.lum_at(&im, x, y + 1) << 1;
+                bits |= self.lum_at(&im, x + 1, y + 1) << 4;
+                bits |= self.lum_at(&im, x, y + 2) << 2;
+                bits |= self.lum_at(&im, x + 1, y + 2) << 5;
+                bits |= self.lum_at(&im, x, y + 3) << 6;
+                bits |= self.lum_at(&im, x + 1, y + 3) << 7;
+                bits |= 0x2800;
 
-            let bits = unsafe {
-                // SAFETY: we have a valid braille char
-                // at this point
-                char::from_u32_unchecked(bits)
-            };
-            let braille_char = bits.encode_utf8(&mut output[..]);
+                let bits = unsafe {
+                    // SAFETY: we have a valid braille char
+                    // at this point
+                    char::from_u32_unchecked(bits)
+                };
+                let braille_char = bits.encode_utf8(&mut output[..]);
 
-            w.write_all(braille_char.as_ref())?;
+                w.write_all(braille_char.as_ref())?;
+            }
+            w.write_all(b"\n")?;
         }
+        Ok(())
     }
-    Ok(())
-}
 
-#[inline]
-fn gray_at(im: &RawPixBuf<Nrgba64Be>, x: usize, y: usize) -> u32 {
-    #[inline(always)]
-    fn at(im: &RawPixBuf<Nrgba64Be>, x: usize, y: usize) -> Gray {
-        <_ as ConvertInto<Gray, specialized::No>>::convert_into(im.color_get(x, y))
+    #[inline]
+    fn lum_at(&self, im: &RawPixBuf<Nrgba64Be>, x: usize, y: usize) -> u32 {
+        #[inline(always)]
+        fn at(im: &RawPixBuf<Nrgba64Be>, x: usize, y: usize) -> Gray {
+            <_ as ConvertInto<Gray, specialized::No>>::convert_into(im.color_get(x, y))
+        }
+        let thres = at(im, x, y).y < self.luminance;
+        (thres != self.inverted) as u32
     }
-    at(im, x, y).y as u32
 }
